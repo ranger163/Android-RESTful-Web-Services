4d55fe1f63420c523bba6b7337cb57df0da4e2dd
Nassar163 <ahmed.nassar163@gmail.com>
2019-01-06 18:53:49 +0200

    - Adding AsyncTaskLoader - Receiving results on MainThread and displaying it - Editing results before displaying them

--- app/src/main/java/me/inassar/restful/MainActivity.kt
+++ app/src/main/java/me/inassar/restful/MainActivity.kt
@@ -1,7 +1,10 @@
 package me.inassar.restful
 
-import android.os.AsyncTask
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.app.LoaderManager
+import android.support.v4.content.AsyncTaskLoader
+import android.support.v4.content.Loader
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,39 +17,51 @@ class MainActivity : AppCompatActivity() {
         interactions()
     }
 
+    /** Notice that supportLoaderManager is deprecated at API 28,
+     *  and it's recommended to start using ViewModel & LiveData
+     **/
     private fun interactions() {
         runCodeBtn.setOnClickListener {
-            val task = MyAsyncTask()
-            task.execute("String 1", "String 2", "String 3")
+            /**
+             * Use restartLoader instead of initLoader to avoid
+             * bypassing loadInBackground function*/
+            supportLoaderManager.restartLoader(0, null, object : LoaderManager.LoaderCallbacks<String> {
+                override fun onCreateLoader(id: Int, args: Bundle?): Loader<String> {
+                    outputTv.append("Creating loader\n")
+                    return MyTaskLoader((this@MainActivity))
+                }
+
+                override fun onLoadFinished(loader: Loader<String>, data: String?) {
+                    outputTv.append("Loader finished, returned: $data")
+                }
+
+                override fun onLoaderReset(loader: Loader<String>) {
+
+                }
+
+            }).forceLoad()
         }
         clearBtn.setOnClickListener {
             outputTv.text = ""
         }
     }
 
-    inner class MyAsyncTask : AsyncTask<String, String, Void>() {
-
-        override fun doInBackground(vararg params: String?): Void? {
-            params.forEach {
-                publishProgress(it)
+    companion object {
+        private class MyTaskLoader(context: Context) : AsyncTaskLoader<String>(context) {
+            override fun loadInBackground(): String? {
                 Thread.sleep(1000)
+                return "From the loader"
             }
-            return null
-        }
 
-        override fun onProgressUpdate(vararg values: String?) {
-            super.onProgressUpdate(*values)
-            outputTv.append("${values[0]},\n")
+            /**
+             * You could use this function to edit your result before
+             * displaying it.
+             * */
+            override fun deliverResult(data: String?) {
+                val result = "$data , delivered\n"
+                super.deliverResult(result)
+            }
         }
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-        }
-
-        override fun onPostExecute(result: Void?) {
-            super.onPostExecute(result)
-        }
-
     }
 
 }
